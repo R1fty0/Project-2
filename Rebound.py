cc738995c60c4a8cc1201f5bd7cd30409c5ccde1
Mohit Sah <m.sah@surreyschools.ca>
2023-01-17 10:24:50 -0800

    2023-01-17
    - Changes a couple things regarding syntax.

--- Rebound.py
+++ Rebound.py
@@ -34,6 +34,7 @@ EndGame = False  # Ends the Game and loads the End Screen
 
 class Scoreboard:
     def __init__(self, Player1Score, Player2Score, MaxScore, ScoreboardFont):  # Constructor
+        """ Create a scoreboard that keeps track of the scores of both players. """
         self.Player1Score = Player1Score
         self.Player2Score = Player2Score
         self.MaxScore = MaxScore
@@ -43,22 +44,27 @@ class Scoreboard:
     Player2ScoreText = "Text"  # Player 2's Score
     PlayerThatWon = 0  # Which Player won the match
 
-    def GetPlayerThatWon(self):  # Getter
+    def determine_which_player_won(self):  # Getter
+        """ Returns which player won the match."""
         return self.PlayerThatWon  # Returns the Player that won the match
 
-    def UpdateScore(self):  # Updates the Score for Both Players
-        self.Player1ScoreText = self.ScoreBoardFont.render(str(self.Player1Score), True, White.GetColor(), Sliver.GetColor())
-        self.Player2ScoreText = self.ScoreBoardFont.render(str(self.Player2Score), True, White.GetColor(), Sliver.GetColor())
+    def update_score(self):
+        """ Updates the  score of both players."""
+        self.Player1ScoreText = self.ScoreBoardFont.render(str(self.Player1Score), True, White.get_color(), Sliver.get_color())
+        self.Player2ScoreText = self.ScoreBoardFont.render(str(self.Player2Score), True, White.get_color(), Sliver.get_color())
 
-    def DrawScore(self):  # Visually updates the Score on Screen for Both Players
+    def draw_score(self):  # Visually updates the Score on Screen for Both Players
+        """ Displays the score of both players on screen.  """
         Window.blit(self.Player1ScoreText, (WindowWidth/4, WindowHeight/8))
         Window.blit(self.Player2ScoreText, (WindowWidth/1.33, WindowHeight / 8))
 
-    def ScoreKeeping(self):  # Calls both Score Management methods
-        self.UpdateScore()
-        self.DrawScore()
+    def score_keeping(self):  # Calls both Score Management methods
+        """ Calls all relevant methods for score keeping."""
+        self.update_score()
+        self.draw_score()
 
-    def AddScore(self, PlayerThatScored):  # Adds a point to a player's score depending on the player that scored the point
+    def add_score(self, PlayerThatScored):
+        """ Adds a point to a player's score depending on the player that scored.  """
         if PlayerThatScored == 1:
             self.Player1Score = self.Player1Score + 1
             print(str(self.Player1Score) + " point(s)")
@@ -67,20 +73,22 @@ class Scoreboard:
             self.Player2Score = self.Player2Score + 1
             print(str(self.Player2Score) + " point(s)")
 
-    def MonitorScore(self):  # Checks to see if either Player has hit the maximum amount of points, and ends the game if one of them did.
+    def monitor_score(self):
+        """ Checks to see if either Player has hit the maximum amount of points, and ends the game if one of them did. """
         global EndGame
 
         if self.Player1Score >= self.MaxScore:
-            self.ResetScore()
+            self.reset_score()
             self.PlayerThatWon = 1
             EndGame = True
 
         if self.Player2Score >= self.MaxScore:
-            self.ResetScore()
+            self.reset_score()
             self.PlayerThatWon = 2
             EndGame = True
 
-    def ResetScore(self):  # Resets the Scores of Both Players
+    def reset_score(self):
+        """  Resets the scores of both players. """
         self.Player1Score = 0
         self.Player2Score = 0
 
@@ -92,15 +100,18 @@ class Scoreboard:
 
 class Image:  # Since All Sprites are Images, this helps to make images more accessible for each game object. Also enables each image to be scaled based on an X and Y divisor
     def __init__(self, image, imageXSizeDivisor, imageYSizeDivisor):  # Constructor
+        """ Class that contains all relevant parameters required to create an image. """
         self.image = image
         # Divisors are based on the game's base resolution = 400 x 400
         self.imageXSizeDivisor = imageXSizeDivisor
         self.imageYSizeDivisor = imageYSizeDivisor
 
-    def GetImage(self):  # Returns Each Image
+    def get_image(self):
+        """ Returns an image upon request. """
         return self.image
 
-    def ScaleImage(self):  # Scales Each Image
+    def scale_image(self):
+        """ Scales an image upon request. """
         self.image = pygame.transform.scale(self.image, (WindowWidth/self.imageXSizeDivisor, WindowHeight/self.imageYSizeDivisor))
 
 
@@ -111,6 +122,7 @@ class Image:  # Since All Sprites are Images, this helps to make images more acc
 
 class Ball(Image):
     def __init__(self, image, spawnX, spawnY, imageXSizeDivisor, imageYSizeDivisor, minBallSpeed, maxBallSpeed):  # Constructor
+        """ Contains all necessary logic for a working ball that can bounce off walls. """
         super().__init__(image, imageXSizeDivisor, imageYSizeDivisor)
         self.image = image
         self.spawnX = spawnX
@@ -121,8 +133,9 @@ class Ball(Image):
     xSpeed = 0  # Ball's Speed along the X-Axis
     ySpeed = 0  # Ball's Speed along the Y-Axis
 
-    def SetStartSpeedAndDirection(self):
-        Direction = random.randrange(self.minBallSpeed, self.maxBallSpeed)  # Determines a random starting direction and speed of the ball
+    def set_starting_parameters(self):
+        """ Determines a random starting direction and speed of the ball. """
+        Direction = random.randrange(self.minBallSpeed, self.maxBallSpeed)
 
         if Direction <= self.minBallSpeed:
             DirectionMultiplier = 1
@@ -134,13 +147,14 @@ class Ball(Image):
         self.ySpeed = random.randrange(self.minBallSpeed, self.maxBallSpeed) * DirectionMultiplier
         print(self.ySpeed)
 
-    def GetSpeed(self, Task):
+    def get_ball_speed(self, Task):
         if Task == 1:
             return self.xSpeed
         elif Task == 2:
             return self.ySpeed
 
-    def BallMovement(self, Ball_Rect, Players):  # Movement and Collision Detection
+    def movement(self, Ball_Rect, Players):
+        """ Handles the ball's movement and collision detection. """
 
         # Movement
         Ball_Rect.x += self.xSpeed  # Moves Ball along the X-axis
@@ -156,29 +170,30 @@ class Ball(Image):
                 self.xSpeed *= -1.1
                 print(self.xSpeed)
 
-    def BallScoreKeeping(self, Ball_Rect):  # Keeps track of the ball's position, and adds a point if a player's scores.
+    def ball_score_keeping(self, Ball_Rect):
+        """ Keeps track of the ball's position, and adds a point if a player's scores. """
 
         if Ball_Rect.left <= 0:  # Ball has moved past the left side of the game window
 
-            Scoreboard.AddScore(2)  # Add a point to Player 2's score
+            Scoreboard.add_score(2)  # Add a point to Player 2's score
 
             # Resets Ball location for another round
             print("Player 2 has Scored")
             self.xSpeed = 0
             self.ySpeed = 0
             Ball_Rect.center = (float(WindowWidth) / 2, float(WindowHeight) / 2)
-            self.SetStartSpeedAndDirection()
+            self.set_starting_parameters()
 
         elif Ball_Rect.right >= WindowWidth:  # Ball has moved past right side of the game window
 
-            Scoreboard.AddScore(1)   # Add a point to Player 1's score
+            Scoreboard.add_score(1)   # Add a point to Player 1's score
 
             # Resets Ball location for another round
             print("Player 1 has Scored")
             self.xSpeed = 0
             self.ySpeed = 0
             Ball_Rect.center = (float(WindowWidth) / 2, float(WindowHeight) / 2)
-            self.SetStartSpeedAndDirection()
+            self.set_starting_parameters()
 
 
 """
@@ -188,9 +203,11 @@ class Ball(Image):
 
 class Color:
     def __init__(self, color):  # Constructor
+        """ Create a color based off of 3 values (Red, Green, Blue). """
         self.color = color  # Tuple containing Color RGB Values
 
-    def GetColor(self):  # Getter
+    def get_color(self):  # Getter
+        """ Returns a color upon request. """
         return self.color  # Returns the color tuple
 
 
@@ -201,6 +218,7 @@ class Color:
 
 class Player(Image):
     def __init__(self, image, spawnX, spawnY, upKey, downKey, speed, imageXSizeDivisor, imageYSizeDivisor):  # Constructor
+        """ Creates a player that can move vertically across the screen and can interact with the game world."""
         super().__init__(image, imageXSizeDivisor, imageYSizeDivisor)
         self.spawnX = spawnX
         self.spawnY = spawnY
@@ -210,9 +228,10 @@ class Player(Image):
 
     # Movement Booleans
     CanMoveUp = True  # Allows the player to move up
-    CanMoveDown = True # Allows the player to move down
+    CanMoveDown = True   # Allows the player to move down
 
-    def PlayerMovement(self, Player_Rect):  # Player Movement and Collision Detection
+    def movement(self, Player_Rect):
+        """ Handles Player movement and collision detection. """
 
         # Checks if Player has hit Top of Window
         if Player_Rect.top <= 0:
@@ -226,7 +245,7 @@ class Player(Image):
         elif Player_Rect.bottom <= WindowHeight:
             self.CanMoveDown = True
 
-        GetKeyDown = pygame.key.get_pressed()  # An event that is triggered, a kin to the "Input.GetKey()" method from Unity (it is a method right?)
+        GetKeyDown = pygame.key.get_pressed()  # An event that is triggered, akin to the "Input.GetKey()" method from Unity (it is a method right?)
 
         if GetKeyDown[self.upKey] and self.CanMoveUp:  # Moves Player Up
             Player_Rect.y -= self.speed
@@ -247,19 +266,19 @@ White = Color((255, 255, 255))
 # Players
 Player1 = Player(pygame.image.load(os.path.join("Assets", "Player1.png")),
                  WindowWidth / 40, WindowHeight / 2.6, pygame.K_w, pygame.K_s, 8, 40, 4)
-Player1.ScaleImage()
+Player1.scale_image()
 
 Player2 = Player(pygame.image.load(os.path.join("Assets", "Player2.png")),
                  WindowWidth / 1.0526, WindowHeight / 2.6, pygame.K_UP, pygame.K_DOWN, 8, 40, 4)
-Player2.ScaleImage()
+Player2.scale_image()
 
 # Mid Line
 MidLine = Image(pygame.image.load(os.path.join("Assets", "MiddleLine.png")), 80, 1)
-MidLine.ScaleImage()
+MidLine.scale_image()
 
 # Ball
 Ball = Ball(pygame.image.load(os.path.join("Assets", "Ball.png")), WindowWidth / 2, WindowHeight / 2, 16.6, 16.6, 2, 3)
-Ball.ScaleImage()
+Ball.scale_image()
 
 # Score Board
 Scoreboard = Scoreboard(0, 0, 5, pygame.font.SysFont("Comic Sans", int(WindowWidth/20), False, False))
@@ -270,9 +289,9 @@ Scoreboard = Scoreboard(0, 0, 5, pygame.font.SysFont("Comic Sans", int(WindowWid
 
 
 def Graphics(Player1_Rect, Player2_Rect, MidLine_Rect, Ball_Rect):  # All the Visuals seen on screen is "drawn" here
-    Window.fill(Sliver.GetColor())  # Background
+    Window.fill(Sliver.get_color())  # Background
 
-    Scoreboard.ScoreKeeping()  # Score Board
+    Scoreboard.score_keeping()  # Score Board
 
     Window.blit(Player1.image, (Player1_Rect.x, Player1_Rect.y))  # Player 1
 
@@ -291,8 +310,9 @@ def Graphics(Player1_Rect, Player2_Rect, MidLine_Rect, Ball_Rect):  # All the Vi
 
 
 def Run():  # Basically the Unity Update Method - all the main game logic runs here.
+    """ The core game loop. All actions that happen in the game occur in this function. """
 
-    # Rect = Essentially pygame's equivalent of GameObjects from Unity or Actors from Greenfoot. Rects store values are really useful for collision detection and movement
+    # Rect = Essentially pygame's equivalent of GameObjects from Unity or Actors from Greenfoot. A rect stores values are really useful for collision detection and movement
 
     Players = []  # List that store Players
 
@@ -312,7 +332,7 @@ def Run():  # Basically the Unity Update Method - all the main game logic runs h
     Ball_Rect = pygame.Rect(WindowWidth / 2 - Ball.image.get_width(), WindowHeight / 2 - Ball.image.get_height(),
                             Ball.image.get_width(), Ball.image.get_height())  # Creates Ball Rect
 
-    Ball.SetStartSpeedAndDirection()  # Generates a starting speed and direction for the ball when the game initally begins
+    Ball.set_starting_parameters()  # Generates a starting speed and direction for the ball when the game initially begins
 
     global EndGame
 
@@ -327,13 +347,13 @@ def Run():  # Basically the Unity Update Method - all the main game logic runs h
 
         Graphics(Player1_Rect, Player2_Rect, MidLine_Rect, Ball_Rect)  # Drawing Visuals
 
-        Player1.PlayerMovement(Player1_Rect)  # Player 1 Movement and Collision Detection
-        Player2.PlayerMovement(Player2_Rect)  # Player 2 Movement and Collision Detection
+        Player1.movement(Player1_Rect)  # Player 1 Movement and Collision Detection
+        Player2.movement(Player2_Rect)  # Player 2 Movement and Collision Detection
 
-        Ball.BallMovement(Ball_Rect, Players)  # Ball Movement and Collision Detection
-        Ball.BallScoreKeeping(Ball_Rect)  # Ball Score Keeping
+        Ball.movement(Ball_Rect, Players)  # Ball Movement and Collision Detection
+        Ball.ball_score_keeping(Ball_Rect)  # Ball Score Keeping
 
-        Scoreboard.MonitorScore()  # Scoreboard Score Keeping
+        Scoreboard.monitor_score()  # Scoreboard Score Keeping
 
     EndGame = False
 
@@ -344,6 +364,7 @@ def Run():  # Basically the Unity Update Method - all the main game logic runs h
 
 
 def EndGameScreen(PlayerThatWon):
+    """ Creates the visuals for the Game Over screen, which displays which player won and instructions on what to do next. """
     # Create Fonts for UI
     WinnerFont = pygame.font.SysFont("Century Gothic", int(WindowWidth / 20), True, False)
     PromptFont = pygame.font.SysFont("Comic Sans MS", int(WindowHeight / 27), False, False)
@@ -351,16 +372,16 @@ def EndGameScreen(PlayerThatWon):
     ScreenIsRunning = True
     while ScreenIsRunning:
 
-        Window.fill(Sliver.GetColor())  # Background Color
+        Window.fill(Sliver.get_color())  # Background Color
 
         # Create Text for UI
-        PromptLabel = PromptFont.render("Press any mouse button to continue!", True, 1, Sliver.GetColor())
+        PromptLabel = PromptFont.render("Press any mouse button to continue!", True, 1, Sliver.get_color())
         WinnerLabel = "none"
 
         if PlayerThatWon == 1:
-            WinnerLabel = WinnerFont.render("Player 1 Won the Match!", True, 1, Sliver.GetColor())
+            WinnerLabel = WinnerFont.render("Player 1 Won the Match!", True, 1, Sliver.get_color())
         elif PlayerThatWon == 2:
-            WinnerLabel = WinnerFont.render("Player 2 Won the Match!", True, 1, Sliver.GetColor())
+            WinnerLabel = WinnerFont.render("Player 2 Won the Match!", True, 1, Sliver.get_color())
 
         # Display UI on Screen
         Window.blit(PromptLabel,
@@ -385,6 +406,8 @@ def EndGameScreen(PlayerThatWon):
 
 
 def MainMenu():
+    """ Create a main menu screen with a title, instructions and the name of the author."""
+
     # Create Fonts for UI
     TitleFont = pygame.font.SysFont("Century Gothic", int(WindowWidth/20), True, False)
     PromptFont = pygame.font.SysFont("Comic Sans MS", int(WindowHeight/27), False, False)
@@ -393,12 +416,12 @@ def MainMenu():
     MenuIsRunning = True
     while MenuIsRunning:
 
-        Window.fill(Sliver.GetColor())
+        Window.fill(Sliver.get_color())
 
         # Create Text for UI
-        TitleLabel = TitleFont.render("Rebound", False, 1, Sliver.GetColor())
-        PromptLabel = PromptFont.render("Press any mouse button to begin!", False, 1, Sliver.GetColor())
-        MadeByLabel = MadeByFont.render("Made by Mohit Sah.", False, 1, Sliver.GetColor())
+        TitleLabel = TitleFont.render("Rebound", False, 1, Sliver.get_color())
+        PromptLabel = PromptFont.render("Press any mouse button to begin!", False, 1, Sliver.get_color())
+        MadeByLabel = MadeByFont.render("Made by Mohit Sah.", False, 1, Sliver.get_color())
 
         # Display UI Text on Screen
         Window.blit(PromptLabel,
@@ -416,7 +439,7 @@ def MainMenu():
 
             if event.type == pygame.MOUSEBUTTONDOWN:  # Runs the game if the user clicks any of the mouse buttons
                 Run()  # Main Game
-                EndGameScreen(Scoreboard.GetPlayerThatWon())  # End Screen
+                EndGameScreen(Scoreboard.determine_which_player_won())  # End Screen
 
     pygame.quit()  # Quits the Application
 
